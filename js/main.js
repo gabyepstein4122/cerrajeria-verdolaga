eff2b602665b758531501027b8d89c5a4fd7abdf
Gabriel Epstein <gabrielepstein4122@gmail.com>
2023-07-24 22:11:28 -0300

    agrege js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+const html = '<a href="https://api.whatsapp.com/send?phone=5493514088801" class="wspBoton" target="_blank"><img src="images/wsp.png" alt="WhatsApp" width="60px" class="rounded-5"></a>'
 const mensaje = () => {
-    Swal.fire("PODEMOS SOLUCIONARLO")
+    Swal.fire({
+        title: 'Puedo solucionarlo! contactame' + html,
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        },
+        confirmButtonText: html
+    })
 };
 const boton = document.getElementById("boton");
-boton.onclick(mensaje);
+boton.addEventListener("click",mensaje);
